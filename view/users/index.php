ffcf1240c77cfd17fd6821f7e7039735f0d02cdf
Merijn <99054535@mydavinci.nl>
2020-06-19 12:07:48 +0200

    don't infinite loop

--- view/users/index.php
+++ view/users/index.php
@@ -2,16 +2,20 @@
     <h1>Welkom op bij de users</h1>
     <p>Je bent nu in users/index</p>
 </div>
-<?php   foreach($users as $result){
+<?php   
    echo "<table class=table>";
    echo "<thead>";
    echo  "<tr>";
    echo     "<th>";
    echo     "<th>Full Name</th>";
-   echo     "<th>Last Name</th>";
+   echo     "<th>Age</th>";
+   echo     "<th>Email</th>";
+   echo     "<th>Phone</th>";
+
    echo   "</tr>";
    echo "</thead>";
    echo "<tbody>";
+   foreach($users as $result){
    echo   "<tr>";
    echo     "<th scope='row'>1</th>";
    echo     "<td>" . $result['full_name'] . "</td>"; 
