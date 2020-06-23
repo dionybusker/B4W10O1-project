3eeb22f6216cec2845c2ab39b093b3da909c64b5
Diony <dionybusker@gmail.com>
2020-06-23 22:52:02 +0200

    andere styling in games/index
    table weggehaald, nu is er een hover + shadow effect op de images

--- view/games/index.php
+++ view/games/index.php
@@ -1,38 +1,15 @@
 <a class="my-2 btn btn-custom-blue rounded-0" href="<?=URL?>games/create">voeg een game toe</a>
 
-<div class="col-12 row">
+<div class="row col-12 justify-content-center">
 
-    <table class="table">
-        <tr>
-            <th></th>
-            <th>Game</th>
-        
-            <th>Plekken</th>
-            <th></th> <!-- detail page -->
-            <th></th> <!-- edit page -->
-            <th></th> <!-- delete page -->
-        </tr>
-        <?php foreach ($games as $result) { ?>
-            <tr>
-                <td>
-                    <a href="#">
-                        <img class="img-fluid" src="<?php echo URL ?>img/games/<?php echo $result["img"] ?>" alt="">
-                    </a>
-                </td>
-                <td><?php echo $result["game_name"] ?></td>
-               
-           
-                <td><?php echo $result["total_spots"] ?></td>
-                <td>
-                    <a class="p-1 btn btn-custom-blue" href="<?php echo URL ?>games/detail/<?= $result['id'] ?>">
-                        <i class="fas fa-gamepad"></i>
-                    </a>
-                </td>
-                <td>
-                
-                </td>
-            </tr>
-        <?php } ?>
-    </table>
+    <?php foreach ($games as $result) { ?>
+        <div class="col-3 card border-0 img-container m-2 p-0">
+            <img class="img-fluid img-game custom-wh-img" src="<?php echo URL ?>img/games/<?php echo $result["img"] ?>" alt="">
+            <div class="info-game">
+                <h4><?php echo $result["game_name"] ?></h4>
+                <a class="p-1 btn btn-custom-blue" href="<?php echo URL ?>games/detail/<?= $result['id'] ?>"><i class="fas fa-gamepad"></i></a>
+            </div>
+        </div>
+    <?php } ?>
 
 </div>
