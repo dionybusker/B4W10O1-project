a295c47a1a6f5c7ddb0bf995f4ddf18fa26f819e
Diony <dionybusker@gmail.com>
2020-06-23 11:47:59 +0200

    styling met bootstrap

--- view/games/create.php
+++ view/games/create.php
@@ -1,71 +1,42 @@
-<h1>voeg een game toe</h1>
-<form name="create" method="post" action="store">
-	<!-- bouw hier je formulier -->
-	
-
-
-
-
-<div class="form-group">
-<label for="">game</label>
-<input type="text" name="game_name" class="form-control" required>
+<div class="col-12 p-0">
+    <h1>voeg een game toe</h1>
 </div>
 
-<div class="form-group">
-<label for="">img</label>
-<input type="text" name="img" class="form-control" required>
-</div>
+<form method="post" action="store">
+    <!-- bouw hier je formulier -->
+
+    <label class="col-4">Game title</label>
+    <input class="col-4" type="text" name="game_name">
+
+    <label class="col-4">Image</label>
+    <input class="col-4" type="text" name="img">
    
-<div class="form-group">
-<label for="">developer</label>
-<input type="text" name="developer" class="form-control" required>
-</div>
+    <label class="col-4">Developer</label>
+    <input class="col-4" type="text" name="developer">
 
-<div class="form-group">
-<label for="">publisher</label>
-<input type="text" name="publisher" class="form-control" required>
-</div>
+    <label class="col-4">Publisher</label>
+    <input class="col-4" type="text" name="publisher">
 
-<div class="form-group">
-<label for="">genre</label>
-<select name="genre" id="genre">
-<?php foreach ($genres as $genre) { ?>
-  <option value="<?php echo $genre["genre_id"] ?>"><?php echo $genre["genre"] ?></option>
-<?php } ?>
-</select>
-</div>
+    <label class="col-4" for="genre">Genre</label>
+    <select class="col-4" name="genre" id="genre">
+        <?php foreach ($genres as $genre) { ?>
+            <option value="<?php echo $genre["genre_id"] ?>"><?php echo $genre["genre"] ?></option>
+        <?php } ?>
+    </select>
 
-<div class="form-group">
-<label for="">platform</label>
-<select name="platform" id="platform">
-<?php foreach ($platforms as $platform) { ?>
-  <option value="<?php echo $platform["platform_id"] ?>"><?php echo $platform["platform"] ?></option>
-<?php } ?>
-</select>
-</div>
+    <label class="col-4" for="platform">Platform</label>
+    <select class="col-4" name="platform" id="platform">
+        <?php foreach ($platforms as $platform) { ?>
+            <option value="<?php echo $platform["platform_id"] ?>"><?php echo $platform["platform"] ?></option>
+        <?php } ?>
+    </select>
 
-<div class="form-group">
-<label for="">aantal plekken</label>
-<input type="text" name="total_spots" class="form-control" required>
-</div>
+    <label class="col-4">How many spots</label>
+    <input class="col-4" type="text" name="total_spots">
 
-<div class="form-group">
-<label for="">description</label>
-<textarea type="text" name="description" class="form-control" required></textarea>
-</div>
+    <label class="col-4">Description</label>
+    <textarea class="col-4" type="text" name="description"></textarea>
 
-
-
-
-
-
-
-<input type="submit" name="create">
-  
-
-
-   
-     
-
+    <input class="col-4 my-2 btn btn-secondary rounded-0" type="submit" name="create">
 </form>
 
