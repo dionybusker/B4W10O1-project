4cee7458f4c1b267f3f6e4d75cb0c87530d5ad61
kimberly <99051407@mydavinci.nl>
2020-06-23 13:19:55 +0200

    detail

--- view/games/detail.php
+++ view/games/detail.php
@@ -16,34 +16,34 @@
         <th></th> <!-- edit page -->
         <th></th> <!-- delete page -->
     </tr>
-    <?php foreach ($game as $result) { ?>
+    
         <tr>
 			<td>
 				<a href="#">
-					<img class="img-fluid" src="<?php echo URL ?>img/<?php echo $result["img"] ?>" alt="">
+					<img class="img-fluid" src="<?php echo URL ?>img/<?php echo $game["img"] ?>" alt="">
 				</a>
 			</td>
-            <td><?php echo $result["game_name"] ?></td>
-            <td><?php echo $result["developer"] ?></td>
-            <td><?php echo $result["publisher"] ?></td>
-            <td><?php echo $result["genre"] ?></td>
-            <td><?php echo $result["platform"] ?></td>
-            <td><?php echo $result["total_spots"] ?></td>
+            <td><?php echo $game["game_name"] ?></td>
+            <td><?php echo $game["developer"] ?></td>
+            <td><?php echo $game["publisher"] ?></td>
+            <td><?php echo $game["genre"] ?></td>
+            <td><?php echo $game["platform"] ?></td>
+            <td><?php echo $game["total_spots"] ?></td>
             <td>
               
             </td>
             <td>
-                <a class="p-1 btn btn-secondary" href="<?=URL?>games/edit/<?= $result['id'] ?>">
+                <a class="p-1 btn btn-secondary" href="<?=URL?>games/edit/<?= $game['id'] ?>">
                     <i class="fas fa-edit"></i>
                 </a>
             </td>
             <td>
-                <a class="p-1 btn btn-danger" href="<?=URL?>games/delete/<?= $result['id'] ?>">
+                <a class="p-1 btn btn-danger" href="<?=URL?>games/delete/<?= $game['id'] ?>">
                     <i class="fas fa-trash-alt"></i>
                 </a>
             </td>
         </tr>
-    <?php } ?>
+    
 </table>
 
 	</div>
