8142bf908644474798928905f8ae8705e2f4b191
Diony <dionybusker@gmail.com>
2020-06-26 12:13:40 +0200

    delete werkt in tournaments

--- view/tournaments/delete.php
+++ view/tournaments/delete.php
@@ -3,5 +3,5 @@
 <p>Are you sure that you want to delete tournament #<?php echo $tournaments["tournament_id"] ?>?</p>
 
 <a class="my-2 btn btn-custom-blue rounded-0" href="<?php echo URL ?>tournaments/index">Cancel!</a>
-<a class="my-2 btn btn-custom-red rounded-0" href="<?php echo URL ?>tournaments/destroy/<?php echo $tournaments["tournaments_id"] ?>" onClick="javascript: return confirm('Are you really, really sure?');">Delete!</a>
+<a class="my-2 btn btn-custom-red rounded-0" href="<?php echo URL ?>tournaments/destroy/<?php echo $tournaments["tournament_id"] ?>" onClick="javascript: return confirm('Are you really, really sure?');">Delete!</a>
 
