d082e0787e88a661e4ce5ba9085956382bcc5f72
Diony <dionybusker@gmail.com>
2020-06-26 11:19:52 +0200

    tabel in tournaments/index

--- view/tournaments/index.php
+++ view/tournaments/index.php
@@ -1,6 +1,36 @@
-<h1>Tournaments</h1>
-<p>tournament/index</p>
+<h1>All tournaments in the database</h1>
 
-<?php
-print_r($tournaments);
-?>
+<a class="my-2 btn btn-custom-blue rounded-0" href="<?= URL ?>tournaments/create">Add a new user!</a>
+
+<table class="table">
+    <tr>
+        <th>ID</th>
+        <th>Game</th>
+        <th>Users</th>
+        <th></th>
+        <th></th>
+        <th></th>
+    </tr>
+    <?php foreach ($tournaments as $result) { ?>
+        <tr>
+            <td><?php echo $result["tournament_id"] ?></td>
+            <td><?php echo $result["game_id"] ?></td>
+            <td><?php echo $result["user_id"] ?></td>
+            <td>
+                <a class="p-1 btn btn-custom-blue" href='#'>
+                    <i class="fas fa-user-alt"></i>
+                </a>
+            </td>
+            <td>
+                <a class="p-1 btn btn-secondary" href='<?= URL ?>tournaments/edit/<?= $result['tournaments_id'] ?>'>
+                    <i class='fas fa-edit'></i>
+                </a>
+            </td>
+            <td>
+                <a class='p-1 btn btn-custom-red' href='<?= URL ?>tournaments/delete/<?= $result['tournaments_id'] ?>'>
+                    <i class='fas fa-trash-alt'></i>
+                </a>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
